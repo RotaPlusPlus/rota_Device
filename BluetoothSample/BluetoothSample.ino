60972760f8937581f904d64227948d5625e66e8e
Hiroki Naganuma <hiroki11x@gmail.com>
2018-11-27 20:26:38 +0900

    Update BluetoothSample.ino

--- BluetoothSample/BluetoothSample.ino
+++ BluetoothSample/BluetoothSample.ino
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2018 Hiroki Naganuma.  All rights reserved.
  * See the bottom of this file for the license terms.
  */
 
@@ -127,7 +127,7 @@ void updateHumidity(){
   }
 }
 /*
-   Copyright (c) 2016 Intel Corporation.  All rights reserved.
+   Copyright (c) 2018 Hiroki Naganuma.  All rights reserved.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -138,8 +138,4 @@ void updateHumidity(){
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
